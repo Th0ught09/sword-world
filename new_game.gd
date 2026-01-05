12a259ded2e0712a8ed912d83f8a816cf9fe57f3
Kirk Matt <kirkmatt@proton.me>
2026-01-05 18:03:51 +0000

    basic level changing

--- new_game.gd
+++ new_game.gd
@@ -6,6 +6,6 @@ extends TextureRect
 func _on_gui_input(event:InputEvent) -> void:
 	if event.is_action("click"):
 		print("works")
-		get_tree().change_scene("res://main_scene.tcsn")
+		get_tree().change_scene_to_file("res://level1.tcsn")
 
 	pass # Replace with function body.
