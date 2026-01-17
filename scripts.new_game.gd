af45e120087cefe512aa6eb5e81c210713d15412
Kirk Matt <kirkmatt@proton.me>
2026-01-17 15:42:56 +0000

    removed debugging code

--- scripts.new_game.gd
+++ scripts.new_game.gd
@@ -5,7 +5,6 @@ extends TextureRect
 
 func _on_gui_input(event:InputEvent) -> void:
 	if event.is_action("click"):
-		print("works")
 		get_tree().change_scene_to_file("res://level1.tcsn")
 
 	pass # Replace with function body.
