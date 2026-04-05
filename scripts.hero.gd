644d68d872771a24f40cdb7141c2190b3117f538
Kirk Matt <kirkmatt@proton.me>
2026-04-05 21:28:17 +0100

    added enemy group, hitting no longer kills block

--- scripts.hero.gd
+++ scripts.hero.gd
@@ -28,6 +28,7 @@ func _process(delta):
 
 
 func _on_body_entered(body:Node2D) -> void:
-	body.queue_free()
+	if body.is_in_group("enemy"):
+		body.queue_free()
 	hit.emit()
 	pass # Replace with function body.
