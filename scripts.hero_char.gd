4a909092eaa34713cbca930354e6a0ba4807f5cc
Kirk Matt <kirkmatt@proton.me>
2026-04-29 16:50:20 +0100

    refactored hero script

--- scripts.hero_char.gd
+++ scripts.hero_char.gd
@@ -1,32 +1,38 @@
 extends CharacterBody2D
 
 signal hit
+@export var speed: int = 100
+@export var gravity: int = 100
 
 func _ready():
-	print("starting")
 	pass
 
-func _physics_process(delta):
-	var velocity = Vector2.ZERO
+func get_input():
+	velocity.x = 0
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 100
+		velocity.x += speed
 		$AnimatedSprite2D.flip_h = false
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 100
+		velocity.x -= speed
 		$AnimatedSprite2D.flip_h = true
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 100
+		velocity.y = -speed
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 100
-	if velocity.length() > 0 and Input.is_action_pressed("click"):
+		velocity.y = speed
+	return velocity
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	get_input()
+	if velocity.x != 0 and Input.is_action_pressed("click"):
 		$AnimatedSprite2D.play("attack_walk")
-	elif velocity.length() > 0:
+	elif velocity.x != 0:
 		$AnimatedSprite2D.play("walk")
 	elif Input.is_action_pressed("click"):
 		$AnimatedSprite2D.play("attack")
 	else:
 		$AnimatedSprite2D.stop()
-	position += velocity * delta
+	move_and_slide()
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
 		var collision_object = collision_info.get_collider()
