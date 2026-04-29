133c08d173a97f10fef315e8c55bd23249c9bff3
Kirk Matt <kirkmatt@proton.me>
2026-04-29 16:53:15 +0100

    renamed scripts

--- scripts.hero.gd
+++ scripts.hero.gd
@@ -1,34 +1,40 @@
-extends RigidBody2D
+extends CharacterBody2D
+
 signal hit
+@export var speed: int = 100
+@export var gravity: int = 100
 
 func _ready():
 	pass
 
-func _process(delta):
-	var velocity = Vector2.ZERO
+func get_input():
+	velocity.x = 0
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 100
+		velocity.x += speed
 		$AnimatedSprite2D.flip_h = false
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 100
+		velocity.x -= speed
 		$AnimatedSprite2D.flip_h = true
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 100
+		velocity.y = -speed
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 100
-	if velocity.length() > 0 and Input.is_action_pressed("click"):
+		velocity.y = speed
+	return velocity
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	get_input()
+	if velocity.x != 0 and Input.is_action_pressed("click"):
 		$AnimatedSprite2D.play("attack_walk")
-	elif velocity.length() > 0:
+	elif velocity.x != 0:
 		$AnimatedSprite2D.play("walk")
 	elif Input.is_action_pressed("click"):
 		$AnimatedSprite2D.play("attack")
 	else:
 		$AnimatedSprite2D.stop()
-	position += velocity * delta
-
-
-func _on_body_entered(body:Node2D) -> void:
-	if body.is_in_group("enemy"):
-		body.queue_free()
-	hit.emit()
-	pass # Replace with function body.
+	move_and_slide()
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		var collision_object = collision_info.get_collider()
+		if collision_object.is_in_group("enemy"):
+			collision_object.queue_free()
